d3b4c257f5ca16a1fc1ccf14c07a30a146e0638d
yan.mezinskiy <yan.mezinskiy@sm-analytics.com>
2024-03-29 22:16:47 +0300

    fix rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,15 +7,15 @@ import {
   SITE_DESCRIPTION_RU,
 } from "../consts";
 
-const { lang } = Astro.props;
+// const { lang } = Astro.props;
 
 export async function GET(context) {
   const posts = await getCollection("blog");
-  const TITLE = lang === "ru" ? SITE_TITLE_RU : SITE_TITLE_EN;
-  const DESCRIPTION = lang === "ru" ? SITE_DESCRIPTION_RU : SITE_DESCRIPTION_EN;
+  //const TITLE = lang === "ru" ? SITE_TITLE_RU : SITE_TITLE_EN;
+  // const DESCRIPTION = lang === "ru" ? SITE_DESCRIPTION_RU : SITE_DESCRIPTION_EN;
   return rss({
-    title: TITLE,
-    description: DESCRIPTION,
+    title: SITE_TITLE_EN,
+    description: SITE_DESCRIPTION_EN,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
